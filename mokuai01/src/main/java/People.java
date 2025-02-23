af6c28e0a1cea881300ac3f72fd4d7f33c30af97
litongtongtong <861618044@qq.com>
2025-02-23 12:12:48 +0800

    Update People.java

--- mokuai01/src/main/java/People.java
+++ mokuai01/src/main/java/People.java
@@ -1,9 +1,19 @@
 public class People {
 
-    public static void main(String[] args) {
+    public static void 主干(String[] args) {
         System.out.println("Hello 的点点滴滴的点点滴滴哒哒哒哒哒哒哒哒哒哒哒哒 World!");
         {
             System.out.println("Hello去去去去去去去去去去七七七七七七七七七七 World!");
+            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+             System.out.println("Hello去去去去去去去去去去七七七七七七七七七七 World!");
+            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+             System.out.println("Hello去去去去去去去去去去七七七七七七七七七七 World!");
             System.out.println("Hello World!");
             System.out.println("Hello World!");
             System.out.println("Hello World!");
