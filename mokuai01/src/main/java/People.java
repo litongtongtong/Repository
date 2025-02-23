6a586caaca8accfe56d4579da4033c526a99191d
LiTong <861618044@qq.com>
2025-02-23 12:08:08 +0800

    改变了一些内容

--- mokuai01/src/main/java/People.java
+++ mokuai01/src/main/java/People.java
@@ -1,7 +1,7 @@
 public class People {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello 的点点滴滴的点点滴滴哒哒哒哒哒哒哒哒哒哒哒哒 World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
